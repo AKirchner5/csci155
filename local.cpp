74d456036fc2baaf7814bab38d3e97d0fbb4103b
Adrian <kirchnera@uindy.edu>
2016-04-07 17:00:22 -0400

    original

--- local.cpp
+++ local.cpp
@@ -1,20 +1,29 @@
 // This function shows that local variables do not retain
 // their values between function calls.
 #include <iostream>
+#include <fstream>
 using namespace std; 
 
 // Function prototype
 void showLocal ();
 int main ()
 {
-showLocal ();
+
 showLocal ();
 return 0;
 }
 
 void showLocal ()
 {
+ofstream outputFile;
+// Open an output file.
+outputFile.open("local.txt");
+  
 int localNum = 5; // Local variables
-cout << "localNum is" << localNum << endl;
+  
+outputFile << "localNum is" << localNum << endl;
 localNum = 99;
+//Close the file. 
+  outputFile.close();
+  cout << "Done.\n";  
 }
